c412d540ebd444029ad1f41bb5d9e32791e1d25c
keithhorgan <keith.horgan2@gmail.com>
2020-03-06 18:31:00 +0000

    Removed mongoose encryption and added md5 hashing.

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const Schema = mongoose.Schema;
 const app = express();
@@ -24,11 +24,6 @@ const userSchema = new Schema({
     password: String
 });
 
-userSchema.plugin(encrypt,{
-    secret: process.env.SECRET, 
-    encryptedFields: ["password"]
-});
-
 const User = new mongoose.model("User", userSchema);
 
 app.get("/", function(req, res){
@@ -45,7 +40,7 @@ app.get("/register", function(req, res){
 
 app.post("/register", function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     const newUser = new User({
         email: username,
         password: password
@@ -62,7 +57,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     User.findOne({email: username}, function(err, foundUser){
         if(err){
