43b12d6a3f8e1e08a8445e695339e923fb4dd9c2
keithhorgan <keith.horgan2@gmail.com>
2020-03-07 21:04:34 +0000

    Add sharing secret functionality

--- app.js
+++ app.js
@@ -35,7 +35,8 @@ const userSchema = new Schema({
     username: String,
     password: String,
     googleId: String,
-    facebookId: String
+    facebookId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -117,11 +118,41 @@ app.get("/register", function(req, res){
 });
 
 app.get("/secrets", function(req, res){
+    User.find({"secret": {$ne: null}}, function(err, foundUsers){
+        if(err) {
+            console.log(err);
+        } else {
+            if(foundUsers) {
+                res.render("secrets", {usersWithSecrets: foundUsers});
+            }
+        }
+    });
+});
+
+app.get("/submit", function(req, res){
     if (req.isAuthenticated()){
-        res.render("secrets");
+        res.render("submit");
     } else {
         res.redirect("/login");
     }
+});
+
+app.post("/submit", function(req, res){
+    const submittedSecret = req.body.secret;
+    console.log(req.user);
+
+    User.findById(req.user.id, function(err, foundUser){
+        if(err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                foundUser.secret = submittedSecret;
+                foundUser.save(function(){
+                    res.redirect("/secrets");
+                })
+            }
+        }
+    })
 });
 
 app.post("/register", function(req, res){
